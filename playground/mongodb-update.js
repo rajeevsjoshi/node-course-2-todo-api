6c8b9ec6d29ba54526540030438e0d76f7a1db17
Rajeev Joshi <Rajeev Joshi>
2017-03-22 21:22:59 -0400

    Added POST /todos route and refactor mongoose

---
+++ playground/mongodb-update.js
@@ -0,0 +1,43 @@
+// const MongoClient = require('mongodb').MongoClient;
+
+const {MongoClient, ObjectID} = require('mongodb');
+
+var obj = new ObjectID();
+console.log(obj);
+
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+    if (err) {
+        return console.log('Unable to connect to MongoDB server');
+    }
+    console.log('Connected to MongoDB server');
+
+    // db.collection('Todos').findOneAndUpdate({
+    //     _id: new ObjectID("58d2bad83923ebe9085bcd96")
+    // }, {
+    //     $set: {
+    //         completed: true
+    //     }
+    // }, {
+    //     returnOriginal: false
+    // }).then((result) => {
+    //     console.log(result);
+    // });
+
+    db.collection('Users').findOneAndUpdate({
+        _id: new ObjectID("58d1f6afa09c632a94e35088")
+    }, {
+        $set: {
+            name: "Rajeev"
+        },
+        $inc: {
+            age: 1
+        }
+    }, {
+        returnOriginal: false
+    }).then((result) => {
+        console.log(result);
+    });
+
+    // db.close();
+});
